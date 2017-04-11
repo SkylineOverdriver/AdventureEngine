9085f192c1daf454b04197670d625ec39c96e7ac
jpartalien <warp2enterprise@gmail.com>
2017-04-10 19:10:06 -0700

    Menu and Controls Update
    -Added in basics for changing the control keys
    -Started to work on a rough draft Main Menu
    TO FINISH:
    - Work on the Scripts for the Menu
    - Fix string --> bool error in keychange subroutine

--- Assets/Scripts/Player2D.cs
+++ Assets/Scripts/Player2D.cs
@@ -1,34 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Player2D : Entity2D {
 
-	// Use this for initialization
+	/** Use this for initialization */
+
+
+	/**These variables control the movement keys. They are changeable*/
+	public KeyCode keyNorth = KeyCode.W;
+	public KeyCode keySouth = KeyCode.S;
+	public KeyCode keyEast = KeyCode.D;
+	public KeyCode keyWest = KeyCode.A;
+
 	protected override void Start () 
 	{
 		base.Start ();
 	}
 	
-	// Update is called once per frame
+	/** Update is called once per frame */
 	void Update() 
 	{
-		if (Input.GetKeyDown (KeyCode.W)) 
+		if (Input.GetKeyDown (keyNorth)) 
 		{
 			Move (movementNorth);
 		} 
-		else if (Input.GetKeyDown (KeyCode.S)) 
+		else if (Input.GetKeyDown (keySouth)) 
 		{
 			Move (movementSouth);
 		} 
-		else if (Input.GetKeyDown (KeyCode.A)) 
+		else if (Input.GetKeyDown (keyWest)) 
 		{
 			Move (movementWest);
 		} 
-		else if (Input.GetKeyDown (KeyCode.D)) 
+		else if (Input.GetKeyDown (keyEast)) 
 		{
 			Move (movementEast);
 		}
 	}
-
+	/**This subroutine is called for when the user is changing the UI control button*/
+	/**public void ChangeKey(string Direction)
+	{
+		if (Direction = "North")
+		{
+			Input.GetKeyDown = keyNorth;
+		}
+		else if (Direction = "South")
+		{
+			Input.GetKeyDown = keySouth;
+		}
+		else if (Direction = "East")
+		{
+			Input.GetKeyDown = keyEast;
+		}
+		else if (Direction = "West")
+		{
+			Input.GetKeyDown = keyWest;
+		}
+		else
+		{
+			print ("Change Key Direction Error");
+		}
+	}
+	*/
 }
